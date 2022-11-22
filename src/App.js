093b47e8586f035255c9a5a8920d56811a28036f
hamza-khalid761 <hamzajadoon761@gmail.com>
2022-11-22 16:17:34 +0500

    First Comment

---
+++ src/App.js
@@ -0,0 +1,179 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+      {/* <nav className='flex justify-start m-10'> */}
+      <nav>
+        {/* <img src='/images/Logo.jpg' alt='' />
+      <h1 className='text-sm font-bold m-2'>SOUNDTRACK</h1> */}
+        {/* //nav bar div */}
+
+        {/* <div className='mx-20'>
+        How IT Works
+      </div>
+      <div className='mx-20'>
+        Library
+      </div>
+      <div className='mx-20'>
+        Pricing
+      </div>
+      <div className='mx-32'>
+        Sign Up
+      </div>
+      <div className='mx-10 relative bg-orange-100 rounded-l-lg fill-s'>
+        Login
+      </div>
+   */}
+        <div className='flex flex-row 	justify-between mt-7 '>
+          <div className='flex flex-row gap-x-12' >
+            <img className='ml-28' src='/images/Logo.jpg' alt='' />
+            <h1 className='text-sm font-bold m-2'>SOUNDTRACK</h1>
+            <h2 className='text-sm'>How It Works</h2>
+            <h2 className='text-sm'>Library</h2>
+            <h2 className='text-sm'>Pricing</h2>
+          </div>
+          <div className="flex flex-row gap-x-12" >
+            <h2 className='p-5'>Sign UP</h2>
+            <h2 className='bg-gray-50 rounded-full w-[120px] h-[70px] p-5 text-center -ml-10'>Login</h2>
+          </div>
+        </div>
+      </nav>
+      {/* <div>
+      <h4 className='text-5xl mx-20 mt-28'>Let’s Connect <br></br> with Your Visitors <br></br> in Real Time</h4>
+      <p className='text-sm mx-20 mt-7'>Lorem ipsum dolor sit amet, consectetur adipiscing <br></br> elit, sed do eiusmod tempor incididunt ut labore</p>
+    </div>
+    <div className='float-right max-w-md max-h-96 mr-96 mb-80 pb-20'>
+      <img className='rounded-full pb-32 pt-10 px-19 w-[300px] h-[500px]' src='/images/Img.jpg' alt=''>
+
+      </img>
+
+    </div> */}
+      <div className='flex flex-row justify-evenly'>
+        <div className='-ml-48'>
+          <h4 className='text-5xl  mt-28'>Let’s Connect <br></br> with Your Visitors <br></br> in Real Time</h4>
+          <p className='text-sm mt-7' >Lorem ipsum dolor sit amet, consectetur adipiscing <br></br> elit, sed do eiusmod tempor incididunt ut labore</p>
+        </div>
+        <div className='-ml-48 mt-16'  >
+          <img className='rounded-full pb-40  w-[250px] h-[450px]' src='/images/Img.jpg' alt=''>
+
+          </img>
+
+        </div>
+      </div>
+      <div className='flex flex-row justify-between m-11'>
+
+        <div className='ml-60 -mt-20'>
+          <img className='rounded-b-full w-[250px] h-[250px]' src='/images/img 1.jpg' alt=''>
+
+          </img>
+
+        </div>
+        <div className='bg-white box-content h-10 w-96 p-4 border-4 ...'>
+          <p className='text-xs font-thin mb-5'>Register Using Email Address</p>
+          <h1 className='text-lg font-bold'>samantha@email.com</h1>
+          <button className='float-right rounded-full bg-blue-800'>Register</button>
+        </div>
+        <div className='-mt-36'>
+          <img className='rounded-b-full w-[250px] h-[250px]' src='/images/Img 2.jpg' alt=''>
+
+          </img>
+
+        </div>
+      </div>
+
+    </>
+
+
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/*       
+      // <div class="flex mb-4">
+      //   <div class="w-1/5 bg-gray-500 h-12">
+
+      //   </div>
+      //   <div class="w-1/5 bg-gray-400 h-12">
+
+      //   </div>
+      //   <div class="w-1/5 bg-gray-500 h-12">
+
+      //   </div>
+      //   <div class="w-1/5 bg-gray-400 h-12">
+
+      //   </div>
+      //   <div class="w-1/5 bg-gray-500 h-12">
+          
+      //   </div>
+      // </div> */}
+
+
+// </nav>  );
+
+
+{/* <ul class="flex">
+  <li class="mr-6">
+    <a class="text-blue-500 hover:text-blue-800" href="#">Active</a>
+  </li>
+  <li class="mr-6">
+    <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
+  </li>
+  <li class="mr-6">
+    <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
+  </li>
+  <li class="mr-6">
+    <a class="text-gray-400 cursor-not-allowed" href="#">Disabled</a>
+  </li>
+</ul> */}
+
+
+
+    //   {/* <nav>
+    //   <h1 className="text-sm font-bold p-7 pb-7 m-10 mr-17 ">
+    //   SOUNDTRACK
+    // </h1>
+    // <h1 className='text-sm inline-flex p-7 pb-10 m-10 mr-17'>
+    //   How it Works
+
+    // </h1>
+    //   </nav> */}
+
+    // <nav className='flex justify-start m-10'>
+    //   <img src='/images/Logo.jpg' alt='' />
+    //   <h1 className='text-sm font-bold m-2'>SOUNDTRACK</h1>
+    //   <div class="flex justify-end ..."></div>
+
+  //   <div class="flex justify-start place-items-center">
+  //   <div className='m-10'>
+  //   <img src='/images/Logo.jpg' alt='' />
+  //   <h1 className='text-sm font-bold'>SOUNDTRACK</h1>
+
+  //   </div>
+  //   <div className='m-10'>02</div>
+  //   <div className='m-10'>03</div>
+  // </div>
